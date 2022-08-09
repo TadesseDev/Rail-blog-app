be0427f0a61e92a7017a908d4d57a20131b6d315
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-09 22:37:15 +0300

    Add: indxes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_09_192418) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_09_193220) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_09_192418) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["authorId"], name: "index_comments_on_authorId"
+    t.index ["postId"], name: "index_comments_on_postId"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -27,6 +29,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_09_192418) do
     t.bigint "postId"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["authorId"], name: "index_likes_on_authorId"
+    t.index ["postId"], name: "index_likes_on_postId"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -37,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_09_192418) do
     t.integer "likesCounter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["authorId"], name: "index_posts_on_authorId"
   end
 
   create_table "users", force: :cascade do |t|
