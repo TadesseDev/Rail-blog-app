e11183e0285494868c63be0b97abbfa31f4ef783
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-11 00:23:52 +0300

    Fix: linter issues

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,11 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  def self.update_post_counter user
+  def self.update_post_counter(user)
     user.update(postCount: Post.where(user: user).count)
   end
 
-  def self.most_recent_comments post
+  def self.most_recent_comments(post)
     Comment.where(post: post).order(created_at: :DESC).limit(5)
   end
 end
