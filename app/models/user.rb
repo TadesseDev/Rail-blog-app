0c38012ee8aa113529a8108b92d03f330ff5a835
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-10 23:52:20 +0300

    Create: most_recent_post method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :likes
-
+  def self.most_recent_posts(user)
+    Post.where(user: user).order(created_at: :desc).limit(3)
+  end
 end
