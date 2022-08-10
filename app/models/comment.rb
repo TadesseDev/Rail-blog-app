bc081b781de45c06609fa2e79ce46b5b27289db0
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-11 00:16:51 +0300

    Create: update_comment_counter method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  def self.update_comment_counter post
+    post.update(commentsCount: Comment.where(post: post).count)
+  end
 end
