d4b1cbd6fa90eca45c4882684f3b97f07f049036
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-11 00:06:49 +0300

    Create: update_post_counter method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :likes
+
+  def self.update_post_counter user
+    user.update(postCount: Post.where(user: user).count)
+  end
 end
