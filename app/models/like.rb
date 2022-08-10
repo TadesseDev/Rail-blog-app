7117975df1996b4db43b96aed66b4319de43f553
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-11 00:18:17 +0300

    Create: update_like_counter method

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  def self.update_like_counter post
+    post.update(commentsCount: Like.where(post: post).count)
+  end
 end
