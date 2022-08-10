8980b74b5ff4ba5634d703b2bbeb637dd2ded80c
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-08-10 15:21:43 +0300

    Use: referece in create table for foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,52 +10,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_09_193220) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_10_112657) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.bigint "authorId"
-    t.bigint "postId"
+    t.bigint "Author_id", null: false
+    t.bigint "post_id", null: false
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["authorId"], name: "index_comments_on_authorId"
-    t.index ["postId"], name: "index_comments_on_postId"
+    t.index ["Author_id"], name: "index_comments_on_Author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
-    t.bigint "authorId"
-    t.bigint "postId"
+    t.bigint "AuthorId_id", null: false
+    t.bigint "PostId_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["authorId"], name: "index_likes_on_authorId"
-    t.index ["postId"], name: "index_likes_on_postId"
+    t.index ["AuthorId_id"], name: "index_likes_on_AuthorId_id"
+    t.index ["PostId_id"], name: "index_likes_on_PostId_id"
   end
 
   create_table "posts", force: :cascade do |t|
-    t.integer "authorId"
+    t.bigint "AuthorId_id", null: false
     t.string "title"
     t.text "text"
-    t.integer "comentsCounter"
-    t.integer "likesCounter"
+    t.integer "commentsCount"
+    t.integer "likesCount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["authorId"], name: "index_posts_on_authorId"
+    t.index ["AuthorId_id"], name: "index_posts_on_AuthorId_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "photo"
-    t.text "bio"
-    t.integer "postCount"
+    t.string "Name"
+    t.string "Photo"
+    t.string "Bio"
+    t.string "PostCount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "posts", column: "postId"
-  add_foreign_key "comments", "users", column: "authorId"
-  add_foreign_key "likes", "posts", column: "postId"
-  add_foreign_key "likes", "users", column: "authorId"
-  add_foreign_key "posts", "users", column: "authorId"
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users", column: "Author_id"
+  add_foreign_key "likes", "posts", column: "PostId_id"
+  add_foreign_key "likes", "users", column: "AuthorId_id"
+  add_foreign_key "posts", "users", column: "AuthorId_id"
 end
