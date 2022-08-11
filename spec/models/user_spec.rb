3dce2b546b1fa8a9a5153b406abea02ed9c5f4ed
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-11 22:30:10 +0300

    Create: user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  valid_user=User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.' , postCount: 1);
+    invalid_name_user=User.new(name: nil, photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',postCount: 2);
+    invalid_post_type_user=User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', postCount: 'string');
+    invalid_negative_post_count_user=User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', postCount: -1);
+  context "test valid users" do
+     it 'validate true for valid user' do
+      expect(valid_user).to be_valid
+    end
+  end
+  context "test invalid users" do
+     it 'validate true for valid user' do
+      expect(valid_user).to be_valid
+    end
+    it 'invalidate user with no name' do
+      expect(invalid_name_user).not_to be_valid
+    end
+    it 'invalidate user with string post count' do
+      expect(invalid_post_type_user).not_to be_valid
+    end
+    it 'invalidate user with negative post count' do
+      expect(invalid_negative_post_count_user).not_to be_valid
+    end
+  end
 end
