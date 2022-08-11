9bbe1316edf1d2e033f226012c6847ff5f8e62ea
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-11 20:53:23 +0300

    Create: likesCounter validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ApplicationRecord
 
   validates :title, presence: true, length: {maximum: 250}
   validates :commentsCount, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  validates :likesCount, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
   def self.update_post_counter(user)
     user.update(postCount: Post.where(user: user).count)
   end
