d518b437754b7a9f37be8e6f6c320c7fbb5bdcca
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-11 20:51:18 +0300

    Create: postCount validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :likes
 
   validates :name, presence: true
+  validates :postCount, numericality: { only_integer: true }
+  validates_comparison_of :postCount, greater_than_or_equal_to: 0
   def self.most_recent_posts(user)
     Post.where(user: user).order(created_at: :desc).limit(3)
   end
