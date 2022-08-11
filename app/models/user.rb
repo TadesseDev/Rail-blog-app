9b1fe7c09adae343b7eeb9163f74888e1f7717e7
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-11 20:50:53 +0300

    Create: Name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :likes
+
+  validates :name, presence: true
   def self.most_recent_posts(user)
     Post.where(user: user).order(created_at: :desc).limit(3)
   end
