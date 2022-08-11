35313b7151cc9e38dcd1c75e91d52a503d9149ab
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-11 23:19:36 +0300

    Fix: linter issues

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-    valid_user = User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
+  valid_user = User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
                         postCount: 1)
-    valid_post = Post.new(user: valid_user, title: 'title', text: 'this is post text', commentsCount: 11,
+  valid_post = Post.new(user: valid_user, title: 'title', text: 'this is post text', commentsCount: 11,
                         likesCount: 1)
-  comment=Comment.new(user: valid_user, post: valid_post, text: 'Hi Tom!' )
+  comment = Comment.new(user: valid_user, post: valid_post, text: 'Hi Tom!')
 
-    context 'test valid comment' do
+  context 'test valid comment' do
     it 'creates a valid comment with valid information' do
       expect(comment).to be_valid
     end
   end
 
-    context 'save and retrive valid comment' do
+  context 'save and retrive valid comment' do
     it 'save valid comment and retirive it from database' do
       comment.save
       expect(Comment.find(comment.id)).to be_an_instance_of(Comment)
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-      valid_user = User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
+  valid_user = User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
                         postCount: 1)
-    valid_post = Post.new(user: valid_user, title: 'title', text: 'this is post text', commentsCount: 11,
+  valid_post = Post.new(user: valid_user, title: 'title', text: 'this is post text', commentsCount: 11,
                         likesCount: 1)
-  like=Like.new(user: valid_user, post: valid_post )
+  like = Like.new(user: valid_user, post: valid_post)
 
-    context 'test valid like' do
+  context 'test valid like' do
     it 'creates a valid like with valid information' do
       expect(like).to be_valid
     end
   end
 
-    context 'save and retrive valid like' do
+  context 'save and retrive valid like' do
     it 'save valid like and retirive it from database' do
       like.save
       expect(Like.find(like.id)).to be_an_instance_of(Like)
