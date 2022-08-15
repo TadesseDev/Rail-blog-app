c2e62a4c8f35dac6fd649d63d6f2f7f6d0bba0d1
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-15 14:31:31 +0300

    Rename: routes_id accordingly

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'users/', to: 'users#index'
-  get 'users/:id/posts/:id', to: 'users#post'
-  get 'users/:id/posts', to: 'users#posts'
+  get 'users/:user_id/posts/:id', to: 'users#post'
+  get 'users/:user_id/posts', to: 'users#posts'
   get 'users/:id', to: 'users#user'
   # Defines the root path route ("/")
   # root "articles#index"
