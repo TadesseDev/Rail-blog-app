4c394ba7151936cb454c7aa1ec904c7ea7d95592
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-15 20:11:05 +0300

    Update: route according to review suggestions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'users/', to: 'users#index'
-  get 'users/:user_id/posts/:id', to: 'users#post'
-  get 'users/:user_id/posts', to: 'users#posts'
-  get 'users/:id', to: 'users#user'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
