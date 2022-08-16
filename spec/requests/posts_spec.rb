47f14e1025e83293ddaf0a70202f3b742838e645
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-16 21:00:26 +0300

    Add: status test for all Actions

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
-  describe "GET /users" do
-    it "returns http success" do
-      get "/posts/users"
-      expect(response).to have_http_status(:success)
+  describe "GET /index" do
+    before(:example) {get users_path}
+    it "response is ok for index" do
+      expect(response).to have_http_status(:ok)
+    end
+  end
+  describe "show action" do
+    it "response is ok for show" do
+      get ("/users/:user_id/posts/:id")
+      expect(response).to have_http_status(:ok)
     end
   end
 
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-
+  describe "GET /index" do
+    before(:example) {get users_path}
+    it "response is ok for index" do
+      expect(response).to have_http_status(:ok)
+    end
+  end
+  describe "show action" do
+    it "response is ok for show" do
+      get ("/users/:user_id")
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
