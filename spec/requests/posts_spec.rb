ced3e752cef709b5c54079f69732ec399c781f5b
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-16 21:05:32 +0300

    Test: template beeing renderd

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -6,11 +6,17 @@ RSpec.describe "Posts", type: :request do
     it "response is ok for index" do
       expect(response).to have_http_status(:ok)
     end
+    it "render the right page" do
+      expect(response).to render_template(:index)
+    end
   end
-  describe "show action" do
+  describe "show action tests" do
+    before(:example) { get ("/users/:user_id/posts/:id")}
     it "response is ok for show" do
-      get ("/users/:user_id/posts/:id")
       expect(response).to have_http_status(:ok)
+    end
+    it "render show template" do
+      expect(response).to render_template(:show)
     end
   end
 
