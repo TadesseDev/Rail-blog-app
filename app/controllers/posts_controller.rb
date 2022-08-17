ae476e50120ac1431d2216b8312c91e270c472d6
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-18 00:53:19 +0300

    Render: user/user_id/post template

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,13 @@
 class PostsController < ApplicationController
+    helper_method :get_top_comments
   def index
     puts params[:id]
+    @user=User.where(id: params[:user_id]).first
     @posts=Post.where(user_id: params[:user_id])
   end
   def show; end
+
+  def get_top_comments(post)
+    Post.most_recent_comments(post)
+  end
 end
