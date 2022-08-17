06ed90fa2e9dc39c18c215ca576d0438fa8699b2
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-17 22:38:23 +0300

    Complete: users and user template and layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,8 @@ class UsersController < ApplicationController
   def index
     @users=User.all
   end
-  def show; end
+  def show
+    @user=User.where(id: params[:id]).first
+    @posts=User.most_recent_posts(@user)
+  end
 end
