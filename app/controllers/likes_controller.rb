8be59a823dc40a05ead157502893a6922d412ea3
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-08-19 11:09:50 +0300

    Create: like controler actions

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,10 @@
 class LikesController < ApplicationController
   def new
+    @like=Like.new
   end
 
   def create
+    Likes.create(user: current_user,post: Post.where(id: params[:user_id]))
   end
 
   def index
