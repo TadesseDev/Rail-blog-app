e2135886bcd9875be53322662eac5f902c791394
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-19 13:52:58 +0300

    Create: like function inside a post/show

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,13 @@
 class LikesController < ApplicationController
-  def new
-    @like=Like.new
-  end
 
   def create
-    Like.create(user: current_user,post: Post.where(id: params[:user_id]).first)
+    post=Post.where(id: params[:post_id]).first
+    user=current_user
+    puts 'this is the find result'
+    puts  Like.where(user: user, post: post).length
+    return if Like.where(user: user, post: post).length>0
+    puts 'saving likes'
+    Like.create(user: user, post: post)
   end
 
   def index
