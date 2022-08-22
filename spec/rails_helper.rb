678f459e830db3120947f665dd746cdc787b3d2c
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-23 00:29:33 +0300

    See all users

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,6 +62,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include Capybara::DSL
 end
 
 # Capybara.register_driver :selenium_chrome do |app|
@@ -69,3 +71,4 @@ end
 # end
 
 # Capybara.javascript_driver = :selenium_chrome
+
