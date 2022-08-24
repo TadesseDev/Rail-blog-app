4baba8716eed3b30665b762a8d2db48317453554
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-25 01:57:09 +0300

    Configure: sign in

--- db/schema.rb
+++ db/schema.rb
@@ -46,9 +46,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_132713) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "photo"
-    t.string "bio"
-    t.integer "postCount"
+    t.string "photo", default: "", null: false
+    t.string "bio", default: "", null: false
+    t.integer "postCount", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
