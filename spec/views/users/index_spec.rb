8920d01a3cff52dcf765e9a496dd45e06f2493a2
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-08-24 18:27:56 +0100

    add spec tests for post show page

--- spec/views/users/index_spec.rb
+++ spec/views/users/index_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe 'users/index.html.erb', type: :system do
+RSpec.describe 'users/index.html.erb', type: :request do
   pending "add some examples to (or delete) #{__FILE__}"
   scenario 'index page' do
     puts
-    visit "http://localhost:3000/"
-    expect(page).to have_content("Tom")
+    visit 'http://localhost:3000/'
+    expect(page).to have_content('Tom')
   end
 end
